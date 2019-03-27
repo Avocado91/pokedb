cfcec8c3b493aac788b61973c738de8c518c86da
T.J <t.ruggles@live.ca>
2019-03-27 01:58:00 -0400

    Setting up api call to pokeapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,39 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+class App extends React.Component {
+    state = {
+        pokemonName: "squirtle",
+    }
+
+    getPokemon = async (e) => {
+        e.preventDefault()
+        e.persist()
+
+        const pokemon = "charmander"
+
+        const apiCall = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        )
+        const response = await apiCall.json()
+
+        this.setState({
+            pokemonName: response.name,
+        })
+
+        console.log(response)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Pokemon</h1>
+                <button onClick={this.getPokemon}>Get Pokemon</button>
+                <p>{this.state.pokemonName}</p>
+            </div>
+        )
+    }
+}
+
+export default App
+
+//test
