a03de016980c82e8f10f7030b251ea919172fddd
T.J <t.ruggles@live.ca>
2019-05-07 16:23:19 -0400

    fixing "crossOrigin" error

--- src/html.js
+++ src/html.js
@@ -19,7 +19,7 @@ export default function HTML(props) {
                     rel="stylesheet"
                     href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
                     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
                 {props.headComponents}
             </head>
